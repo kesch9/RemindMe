c1b330422acb3ff6c6373f1da00c6c6a4ddbb7b0
kesch9 <kashenkovsergej@yandex.ru>
2016-02-10 16:43:50 +0400

    Add Main_Activity

---
+++ app/src/main/java/com/example/user/remindme/Main_Activity.java
@@ -0,0 +1,12 @@
+package com.example.user.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
